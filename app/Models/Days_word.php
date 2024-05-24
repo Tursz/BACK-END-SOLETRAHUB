50341ebfd06ac1706432de3daf73e866780f630d
Tursz <arturbaixa13@gmail.com>
2024-05-24 13:12:09 -0300

    retirando o projeto da pasta api

---
+++ app/Models/Days_word.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Days_word extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'word_1',
+        'word_2',
+        'word_3',
+        'word_4',
+        'word_5',
+        'word_6',
+        'word_7',
+    ];
+}
